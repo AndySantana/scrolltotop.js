f3429c5feef2300af437b3a76802878f6401524a
Joel A. Jaime <joel.alexander.jaime@gmail.com>
2014-08-18 09:01:17 -0400

    Encapsulating, adding default values and extend values

--- src/jquery.scrolltotop.js
+++ src/jquery.scrolltotop.js
@@ -1,48 +1,51 @@
+// protect your code
+(function($, window){
+	$.fn.Up = function( options )
+	{
+		// settings default values
+		var settings = $.extend({}, $.fn.Up.defaults, options);
 
+		// selector
+		var body = $(this);
 
+		//button to display
+		var element = '<span class="up"><img src="img/sprites.png"></span>';
 
-$.fn.Up = function()
-{
-	var body = $(this);
+		$(body).prepend(element);
 
-	var element = '<span class="up"><img src="img/sprites.png"></span>';
+		$("span.up").css({
+			"position" : "fixed",
+			"bottom" : 50,
+			"right" : 30,
+			"background" : "#008080",
+			"color" : "#fff",
+			"padding" : "1em",
+			"cursor" : "pointer",
+			"border-radius" : "3px",
+			"display" : "none"
 
-	$(body).prepend(element);
+		});
 
-	$("span.up").css({
-		"position" : "fixed",
-		"bottom" : 50,
-		"right" : 30,
-		"background" : "#008080",
-		"color" : "#fff",
-		"padding" : "1em",
-		"cursor" : "pointer",
-		"border-radius" : "3px",
-		"display" : "none"
+		// event handler
+		$(".up").on("click",function(){
+			$("html,body").animate({
+				scrollTop : 0
+			},400 );
+		});
 
-	});
+		// window scroll
+		$(window).scroll(function(){
+			if ($(this).scrollTop() > 300){
+				$(".up").slideDown( settings.time );
 
-	$(".up").on("click",function(){
+			}else{
+				$(".up").slideUp( settings.time );
+			}
+		});
+	};
 
-		$("html,body").animate({
-			scrollTop : 0
-		},400 );
-
-	});
-
-	$(window).scroll(function(){
-
-		var defaultTime = 300;
-
-		if ($(this).scrollTop() > 300) 
-		{
-			$(".up").slideDown(defaultTime);
-
-		}
-		else
-		{
-			$(".up").slideUp(defaultTime);
-		}
-	});
-
-};
+	// default values
+	$.fn.Up.defaults = {
+		time: 300
+	};
+})(jQuery, window);
